cdb08f5ee9c5d417627a712447679e65d9c4f3b7
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-08-25 17:37:04 -0500

    Add places urls to project urls

--- nightOut/urls.py
+++ nightOut/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('gAuth.urls')),  # Include aAuth URLs
+    path('api/', include('places.urls')), # Include places URLs
     path('accounts/', include('django.contrib.auth.urls')),
 ]
